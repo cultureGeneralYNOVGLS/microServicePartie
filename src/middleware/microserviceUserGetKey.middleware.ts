69fe1a54d5bc3e9b3a2dbacdfd70fc04c7c0ef0f
CorentinGlrd5 <corentin.guillard@ynov.com>
2021-12-02 05:30:28 +0100

    fix(main): Test fix partie

--- src/middleware/microserviceUserGetKey.middleware.ts
+++ src/middleware/microserviceUserGetKey.middleware.ts
@@ -1,18 +1,24 @@
 module.exports = (req: any, res: any, next: any) => {
+    console.log(7, 'start');
+    
     const fetch = require('node-fetch');
+    console.log(8, 'start');
 
     fetch("http://micro-service-user:3000/api/user/getkey", {
         method: "GET"
     }).then((fetc:any) => {
+        console.log(9, fetc.status);
         if (fetc.status === 404 || fetc.status === 500) {
             res.status(401).json({
                 error: "Data is not good"
             });
         }
         else {
+            console.log(10, fetc.status);
             next();
         }
     }).catch(()=>{
+        console.log(11, 'error');
         res.status(401).json({
             error: "Data is not good"
         });
--- src/middleware/userToken.middleware.ts
+++ src/middleware/userToken.middleware.ts
@@ -1,8 +1,10 @@
 module.exports = (req: any, res: any, next: any) => {
+    console.log(12, 'start');
     console.log("verif Token Partie")
     const fetch = require('node-fetch');
     try {
         const token = req.headers.authorization.split(' ')[1];
+        console.log(13, token);
 
         fetch(`http://micro-service-user:3000/api/user/auth`, {
             method: "POST",
@@ -11,6 +13,7 @@ module.exports = (req: any, res: any, next: any) => {
                 "Content-type": "application/json; charset=UTF-8",
             },
         }).then((fetc:any) => {
+            console.log(14, fetc.status);
             if (fetc.status === 400) {
                 res.status(401).json({
                     error: "Token Invalid"
@@ -21,11 +24,13 @@ module.exports = (req: any, res: any, next: any) => {
             }
         })
         .catch(() => {
+            console.log(15, 'error');
             res.status(401).json({
                 error: "Token Invalid"
             });
           });
     } catch {
+        console.log(16, 'error');
         res.status(401).json({
             error: "Token Invalid"
         });
